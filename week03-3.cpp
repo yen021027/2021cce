e882d95f7f26a5188fea855110312ed916570291
yen021027 <91250422+yen021027@users.noreply.github.com>
2021-09-30 17:37:03 +0800

    Add files via upload

---
+++ week03-3.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+    printf("%d %d %d ", 10 ,20 ,30);
+}
---
+++ week03-5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+
+    printf("輸入第一個數");
+
+    scanf("%d", &A);
+
+    printf("輸入第二個數");
+
+    scanf("%d", &B);
+
+    printf("結果為%d", A+B);
+}
