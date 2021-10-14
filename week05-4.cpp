414c09777159536bf3c596dbf94389f40d5b2947
yen021027 <91250422+yen021027@users.noreply.github.com>
2021-10-14 17:40:33 +0800

    Add files via upload

---
+++ week05-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    if("-2")printf("-2成立\n");
+    if("-1")printf("-1成立\n");
+    if("0")printf("0成立\n");
+    if("1")printf("1成立\n");
+    if("2")printf("2成立\n");
+    if("3")printf("3成立\n");
+    if("4")printf("4成立\n");
+}
