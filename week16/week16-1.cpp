cb41b607a01a5bad2744ca11e88091cd2e72d0f2
yen021027 <brian20021027@gmail.com>
2021-12-30 17:39:34 +0800

    add week16

---
+++ week16/week16-1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i=0;
+	while(i<n){
+		int k=0;
+		while(k<n){
+			if(k<n-i-1) printf(" ");
+			else printf("*");
+			k++;
+		}
+		int space=n-i-1;
+		printf("\n");
+		i++;
+	}
+}
